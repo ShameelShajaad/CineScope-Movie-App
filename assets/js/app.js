ec367c06ecbec14ed52619115dfdd790f2229bf4
ShameelShajaad <shameels1228@gmail.com>
2025-11-24 20:48:21 +0530

    Added API request in js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,13 @@
 console.log("js loded!");
 
-const imdbBtn = document.getElementById("imdbBtn");
-const nameBtn = document.getElementById("nameBtn");
-const searchInput = document.getElementById("searchInput");
+const apiKey = dcd0f9e;
+
+let imdbBtn = document.getElementById("imdbBtn");
+let nameBtn = document.getElementById("nameBtn");
+let searchInput = document.getElementById("searchInput");
+let search_btn = document.getElementById("search_btn");
+
+let search_by = "imdb";
 
 imdbBtn.addEventListener("click", () => {
   searchInput.placeholder = "Enter IMDb ID";
@@ -10,6 +15,7 @@ imdbBtn.addEventListener("click", () => {
   imdbBtn.classList.remove("bg-gray-800");
   nameBtn.classList.remove("bg-purple-600");
   nameBtn.classList.add("bg-gray-800");
+  search_by = "imdb";
 });
 
 nameBtn.addEventListener("click", () => {
@@ -18,4 +24,27 @@ nameBtn.addEventListener("click", () => {
   nameBtn.classList.remove("bg-gray-800");
   imdbBtn.classList.remove("bg-purple-600");
   imdbBtn.classList.add("bg-gray-800");
+  search_by = "name";
 });
+
+search_btn.addEventListener("click", () => {
+  apiCall(searchInput.value);
+});
+
+searchInput.addEventListener("keypress", (e) => {
+  if (e.key === "Enter") {
+    apiCall(searchInput.value);
+  }
+});
+
+async function apiCall(search) {
+  let url = "";
+  if (search_by === "imdb") {
+    url = "https://www.omdbapi.com/?i=" + search + "&apikey=" + apiKey;
+  } else {
+    url = "https://www.omdbapi.com/?t=" + search + "&apikey=" + apiKey;
+  }
+
+  let res = await fetch("url");
+  let data = await res.json();
+}
